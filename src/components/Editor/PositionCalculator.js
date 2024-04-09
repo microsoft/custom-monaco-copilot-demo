ef5d9fc3954ca1cd7b0ce252c3e680cd59344a90
Aymen Furter <aymen.furter@outlook.com>
2024-04-09 14:44:13 +0200

    feat: Add copyright information to files

--- src/components/Editor/PositionCalculator.js
+++ src/components/Editor/PositionCalculator.js
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 class PositionCalculator {
   getLineNumber(xmlString, node) {
     const nodePosition = xmlString.indexOf(node.outerHTML);
